17c309c296c40e52f9fad68a91dab420d00f8821
Julien Noel <jncoweb@gmail.com>
2024-06-10 02:16:58 +0200

    wip changed .env

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -2,7 +2,7 @@ import { Resend } from 'resend'
 import { Email } from '@/components/ContactForm';
 import { NextRequest, NextResponse } from 'next/server';
 
-const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY)
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(req: NextRequest) {
     const { name, email, message } = await req.json();
