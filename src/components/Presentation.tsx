aff6a5ac5ed92fe303ca98d116add1fa33d1885b
Julien Noel <jncoweb@gmail.com>
2024-06-11 21:35:26 +0200

    wip changed style

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -7,25 +7,25 @@ import Image from 'next/image'
 const TITLE = 'Hi I\'m Julien Noel'
 export const Presentation = ():JSX.Element => {
   return (
-      <div className="md:fixed md:w-1/2 h-full py-1 content-around">
+      <div className="md:fixed md:w-1/2 h-fit py-1">
         <h1 className="text-slate-200">{TITLE} 🖐️</h1>
         <p>I am a frontend React Native Developer,<br/>with a strong background in Javascript and Typescript</p>
         <WordRotate
           className="text-xl font-bold text-teal-400 dark:text-white"
           words={["React-Native", "Typescript", "Redux", "React-native Reanimated", "Frontend", "Javascript","React Navigation"]}
           />
-            <div className="flex flex-col justify-between">
-              <Image src='/julien-noel.jpg' 
-                    alt='julien Noel react native developer'
-                    width={250}
-                    height={250}
-                    loading='lazy'
-                    className="rounded-lg h-1/2 shadow-md shadow-teal-400" 
-                    />
-              <ContactIcons />
-              <a href='/resume-julien-noel.pdf' target="_blank"><ShinyButton text="My Resume"/></a>
-            {/* <Technos /> */}
-            </div>
-          </div>      
+        <div className="flex flex-col justify-between">
+          <Image src='/julien-noel.jpg' 
+                alt='julien Noel react native developer'
+                width={250}
+                height={250}
+                loading='lazy'
+                className="rounded-lg h-1/2 shadow-md shadow-teal-400"                 
+                />
+          <ContactIcons />
+          <a href='/resume-julien-noel.pdf' target="_blank"><ShinyButton text="My Resume"/></a>
+        {/* <Technos /> */}
+        </div>
+      </div>      
   );
 }
